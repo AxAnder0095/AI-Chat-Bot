f43fe4f8c14a8d53ad3166e1d98d4f870d028c50
Frenguin <104327737+Frenguin@users.noreply.github.com>
2022-10-23 18:50:29 -0500

    training

--- Scripts/trainingAlexAI.py
+++ Scripts/trainingAlexAI.py
@@ -1,3 +1,34 @@
 import random
 import json
 import pickle
+import numpy as np
+
+import nltk
+#nltk.download('punkt')
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Activation, Dropout
+from tensorflow.python.keras.optimizers import gradient_descent_v2
+
+lemmatizer = WordNetLemmatizer
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '.', '?', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+
+        words.append(word_list)
+        documents.append((word_list, intent['tag']))
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+print(documents)
